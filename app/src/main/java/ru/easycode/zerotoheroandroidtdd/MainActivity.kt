0cbf2f027e4493078dd5b8044d9b7c49d775598c
Vladimir <avg151@ya.ru>
2026-04-30 22:47:23 +0700

    Task10: Solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,41 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var isRemoved = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rootLayout = findViewById<ViewGroup>(R.id.rootLayout)
+        val titleTextView = findViewById<TextView>(R.id.titleTextView)
+        val removeButton = findViewById<Button>(R.id.removeButton)
+
+        if (savedInstanceState != null) {
+            isRemoved = savedInstanceState.getBoolean("isRemoved", false)
+        }
+
+        if (isRemoved) {
+            rootLayout.removeView(titleTextView)
+            removeButton.isEnabled = false
+        }
+
+        removeButton.setOnClickListener {
+            rootLayout.removeView(titleTextView)
+            removeButton.isEnabled = false
+            isRemoved = true
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("isRemoved", isRemoved)
     }
 }
