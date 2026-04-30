a6f2ed273c0e6131d94186b84e713b9dd181ebf9
Vladimir <avg151@ya.ru>
2026-04-30 22:43:31 +0700

    Task9: Solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,42 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var isTextViewRemoved = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rootLayout = findViewById<LinearLayout>(R.id.rootLayout)
+        val titleTextView = findViewById<TextView>(R.id.titleTextView)
+        val removeButton = findViewById<Button>(R.id.removeButton)
+
+        if (savedInstanceState != null) {
+            isTextViewRemoved = savedInstanceState.getBoolean(KEY_REMOVED, false)
+        }
+
+        if (isTextViewRemoved) {
+            rootLayout.removeView(titleTextView)
+        }
+
+        removeButton.setOnClickListener {
+            rootLayout.removeView(titleTextView)
+            isTextViewRemoved = true
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(KEY_REMOVED, isTextViewRemoved)
+    }
+
+    companion object {
+        private const val KEY_REMOVED = "isTextViewRemoved"
     }
 }
