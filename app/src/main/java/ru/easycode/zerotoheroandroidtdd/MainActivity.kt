cc35b71c9e5b31ba105ac6053f8dd32da7a41ffc
Vladimir <avg151@ya.ru>
2026-05-01 15:17:42 +0700

    Task35: Solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -3,9 +3,16 @@ package ru.easycode.zerotoheroandroidtdd
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import ru.easycode.zerotoheroandroidtdd.ui.theme.ZeroToHeroAndroidTDDTheme
 
@@ -18,7 +25,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //todo
+                    var count by rememberSaveable {
+                        mutableStateOf<Count>(Count.Base(min = 0, max = 2, value = 1, step = 1))
+                    }
+
+                    Column {
+                        Text(text = count.toString())
+                        Button(
+                            onClick = { count = count.increment() },
+                            enabled = !count.isMax()
+                        ) {
+                            Text(text = "+")
+                        }
+                        Button(
+                            onClick = { count = count.decrement() },
+                            enabled = !count.isMin()
+                        ) {
+                            Text(text = "-")
+                        }
+                    }
                 }
             }
         }
