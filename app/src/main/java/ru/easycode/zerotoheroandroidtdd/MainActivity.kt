2429eb2c66a752d94dc472f9a84141f7e358d111
Vladimir <avg151@ya.ru>
2026-05-01 15:40:50 +0700

    Task38: Solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,12 +1,24 @@
 package ru.easycode.zerotoheroandroidtdd
 
 import android.os.Bundle
+import android.util.Patterns.EMAIL_ADDRESS
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import ru.easycode.zerotoheroandroidtdd.ui.theme.ZeroToHeroAndroidTDDTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +30,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //todo
+                    var email by rememberSaveable { mutableStateOf("") }
+                    val isValid = EMAIL_ADDRESS.matcher(email).matches()
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        TextField(
+                            value = email,
+                            onValueChange = { email = it },
+                            modifier = Modifier.testTag("emailInputTag")
+                        )
+                        Button(
+                            onClick = { },
+                            enabled = isValid,
+                            modifier = Modifier.testTag("loginButtonTag")
+                        ) {
+                            Text("login")
+                        }
+                    }
                 }
             }
         }
